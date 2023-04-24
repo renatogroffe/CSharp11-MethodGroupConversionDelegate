dea4b3f52afc920f4b99a3fbf8a885d28e42a47e
Renato Groffe <renato.groffe@yahoo.com.br>
2023-04-24 07:31:02 -0300

    C# 11 + Console Application + Improved method group conversion to delegate
    C# 11 + Console Application + Improved method group conversion to delegate

---
+++ ExemploMethodGroupConversion/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.InteropServices;
+using System.Text.Json;
+using ExemploMethodGroupConversion;
+
+Console.WriteLine("***** Testes com C# 11 | Improved Method Group Conversion to Delegate *****");
+Console.WriteLine($"Versao do .NET em uso: {RuntimeInformation
+    .FrameworkDescription} - Ambiente: {Environment.MachineName}- Kernel: {Environment
+    .OSVersion.VersionString}");
+
+Console.WriteLine(
+    $"{nameof(Testes.ListarNumerosImpares)} = {JsonSerializer.Serialize(Testes.ListarNumerosImpares())}");
+
+Console.WriteLine(
+    $"{nameof(Testes.ListarNumerosImparesV2)} = {JsonSerializer.Serialize(Testes.ListarNumerosImparesV2())}");
---
+++ ExemploMethodGroupConversion/Testes.cs
@@ -0,0 +1,18 @@
+﻿namespace ExemploMethodGroupConversion;
+
+public static class Testes
+{
+    private static readonly List<int> Numeros = new List<int>() { 32, 28, 9, 67, 26, 59, 36, 41, 49 };
+
+    public static List<int> ListarNumerosImpares()
+    {
+        return Numeros.Where(n => NumeroImpar(n)).ToList();
+    }
+
+    public static List<int> ListarNumerosImparesV2()
+    {
+        return Numeros.Where(NumeroImpar).ToList();
+    }
+
+    private static bool NumeroImpar(int numero) => numero % 2 != 0;
+}
